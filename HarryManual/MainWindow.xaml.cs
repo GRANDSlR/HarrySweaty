d0b33bbbf50ba77c91e13faeaa2702df9ca7d570
Arthur Lenkovets <Lenkovets.arthur@gmail.com>
2024-06-14 22:40:03 +0300

    login/register logic

---
+++ HarryManual/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using HarryManual;
+using HarryManual.DataAccess;
+using System.Data.Entity;
+using System.IO.Packaging;
+using System.Linq;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace HarryManual
+{
+
+    public partial class MainWindow : Window
+    {
+        private DataBaseContext dbContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dbContext = new DataBaseContext();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Password;
+
+            if (toggleRegistration.IsChecked == true)
+            {
+                Register(username, password);
+            }
+            else
+            {
+                Login(username, password);
+            }
+        }
+
+        private void Login(string username, string password)
+        {
+            User user = dbContext.Users.FirstOrDefault(u => u.Name == username);
+
+            if (user != null && user.PasswordHash == password)
+            {
+                MessageBox.Show("Вы вошли в систему.");
+            }
+            else
+            {
+                MessageBox.Show("Неверное имя пользователя или пароль.");
+            }
+        }
+
+
+        private void Register(string username, string password)
+        {
+            using (var dbContext = new DataBaseContext())
+            {
+                var newUser = new User
+                {
+                    Name = username,
+                    PasswordHash = password
+                };
+
+                dbContext.Users.Add(newUser);
+                dbContext.SaveChanges();
+
+
+                if (dbContext.Users.Any(u => u.Name == username))
+                {
+                    MessageBox.Show("Пользователь успешно зарегистрирован.");
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка при регистрации пользователя.");
+                }
+            }
+        }
+
+
+        /*        private string CalculateHash(string input)
+                {
+                    // Ваш код для вычисления хэша пароля.
+                    // Здесь вы можете использовать, например, алгоритм SHA256.
+                }*/
+    }
+}
